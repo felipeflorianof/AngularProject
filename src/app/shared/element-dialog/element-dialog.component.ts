feaa51532bbee10f664c1b01f53ae57af0232c47
felipeflorianof <felipeflorianof@gmail.com>
2023-01-17 00:55:35 -0300

    Teste técnico - Angular, Procenge

---
+++ src/app/shared/element-dialog/element-dialog.component.ts
@@ -0,0 +1,26 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import { PeriodicElement } from 'src/app/views/home/home.component';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-element-dialog',
+  templateUrl: './element-dialog.component.html',
+  styleUrls: ['./element-dialog.component.css']
+})
+export class ElementDialogComponent implements OnInit{
+  element!: PeriodicElement;
+
+  constructor (
+    @Inject(MAT_DIALOG_DATA) 
+    public data: PeriodicElement,
+    public dialogRef: MatDialogRef<ElementDialogComponent>, 
+  ) {}
+
+  ngOnInit(): void{
+
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
